7a49a64bac54686aaf2edc3203931e666231bef0
crushcitiesff <122638264+crushcitiesff@users.noreply.github.com>
2023-01-13 13:50:40 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "869298701691166720"; // your league ID
-export const leagueName = "$20 Winner Take All"; // your league name
+export const leagueID = "859898238671634432"; // your league ID
+export const leagueName = "Kupp Runnith Over"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
